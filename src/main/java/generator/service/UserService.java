987e7931c7c4311d39e8745346f521750c48dda3
wy <824745026@qq.com>
2024-09-08 16:53:02 +0800

    usercenter first

---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author admin
+* @description 针对表【user(用户表)】的数据库操作Service
+* @createDate 2024-07-24 13:39:02
+*/
+public interface UserService extends IService<User> {
+
+}
